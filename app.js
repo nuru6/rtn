2049aeee4324c1b74acfa577795d6a59026f0fec
NURU <61076356+nuru7595@users.noreply.github.com>
2023-08-26 09:40:06 +0600

    2 Hours Study Update

--- app.js
+++ app.js
@@ -3,71 +3,62 @@ function func() {
     let h = d.getHours();
     let m = d.getMinutes();
     // Morning Shift;
-    if (h == 6) {
+    if (h == 6 || h == 7) {
         document.querySelector(".n1").classList.add("act");
-    } else {
+    } else if (h == 8 || h == 9) {
         document.querySelector(".n1").classList.remove("act");
-    }
-    // Afternoon Shift;
-    if (h == 13) {
-        if (m >= 30) {
-            document.querySelector(".n2").classList.add("act");
-        }
-    } else if (h == 14) {
-        if (m <= 15) {
-            document.querySelector(".n2").classList.add("act");
-        } else if (m <= 25) {
-            document.querySelector(".n2").classList.remove("act");
+        document.querySelector(".n2").classList.add("act");
+    } else if (h == 10) {
+        document.querySelector(".n2").classList.remove("act");
+        if (m < 30) {
             document.querySelector(".b1").classList.add("act");
         } else {
             document.querySelector(".b1").classList.remove("act");
             document.querySelector(".n3").classList.add("act");
         };
-    } else if (h == 15) {
-        if (m <= 10) {
-            document.querySelector(".n3").classList.add("act");
-        } else if (m <= 20) {
-            document.querySelector(".n3").classList.remove("act");
+    }
+    } else if (h == 11) {
+	    document.querySelector(".n3").classList.add("act");
+    } else if (h == 12) {
+        if (m < 30) {
             document.querySelector(".b2").classList.add("act");
         } else {
             document.querySelector(".b2").classList.remove("act");
             document.querySelector(".n4").classList.add("act");
-        }
-    } else if (h == 16) {
-        if (m <= 5) {
+        };
+    } else if (h == 13) {
+            document.querySelector(".n4").classList.add("act");
+    } else if (h == 14) {
+        if (m < 30) {
             document.querySelector(".n4").classList.add("act");
         } else {
             document.querySelector(".n4").classList.remove("act");
-        }
-    }
-    // Evening Shift;
-    if (h == 20) {
-        if (m <= 45) {
-            document.querySelector(".n5").classList.add("act");
-        } else if (m <= 55) {
-            document.querySelector(".n5").classList.remove("act");
+            document.querySelector(".b3").classList.add("act");
+        };
+    } else if (h == 15 || h == 16) {
+        document.querySelector(".b3").classList.remove("act");
+        document.querySelector(".n5").classList.add("act");
+    } else if (h == 17) {
+        document.querySelector(".n5").classList.remove("act");
+        if (m < 30) {
             document.querySelector(".b4").classList.add("act");
         } else {
             document.querySelector(".b4").classList.remove("act");
             document.querySelector(".n6").classList.add("act");
-        }
-    } else if (h == 21) {
-        if (m <= 40) {
+        };
+    } else if (h == 18) {
+        document.querySelector(".n6").classList.add("act");
+    } else if (h == 19) {
+        if (m < 30) {
             document.querySelector(".n6").classList.add("act");
-        } else if (m <= 50) {
+        } else {
             document.querySelector(".n6").classList.remove("act");
             document.querySelector(".b5").classList.add("act");
-        } else {
-            document.querySelector(".b5").classList.remove("act");
-            document.querySelector(".n7").classList.add("act");
-        }
-    } else if (h == 22) {
-        if (m <= 35) {
-            document.querySelector(".n7").classList.add("act");
-        } else {
-            document.querySelector(".n7").classList.remove("act");
-        }
-    }
+        };
+    } else if (h == 20 || h == 21) {
+        document.querySelector(".b5").classList.remove("act");
+        document.querySelector(".n7").classList.add("act");
+    };
 };
 func();
 setInterval(func, 1000);
