4008960145c257068cbb7d8966d87018d1338f86
NURU <nuruzzamannuru7595@gmail.com>
2023-10-17 19:38:18 +0600

    Version 0.1

--- app.js
+++ app.js
@@ -1,81 +1,94 @@
-function func() {
-    const d = new Date();
-    let h = d.getHours();
-    let m = d.getMinutes();
-    //
-    if (h == 6 || h == 7) {
-        document.querySelector(".n1").classList.add("act");
-    } else if (h == 8) {
-        document.querySelector(".n1").classList.remove("act");
-        if (m < 30) {
-            document.querySelector(".b1").classList.add("act");
-        } else {
-            document.querySelector(".b1").classList.remove("act");
-            document.querySelector(".n2").classList.add("act");
-        };
-    } else if (h == 9) {
-        document.querySelector(".n2").classList.add("act");  
-    } else if (h == 10) {
-        if (m < 30) {
-            document.querySelector(".n2").classList.add("act");
-        } else {
-            document.querySelector(".n2").classList.remove("act");
-            document.querySelector(".n3").classList.add("act");
-        };
-    } else if (h == 11) {
-        document.querySelector(".n3").classList.add("act");
-    } else if (h == 12) {
-        document.querySelector(".n3").classList.remove("act");
-        if (m < 30) {
-            document.querySelector(".b2").classList.add("act");
-        } else {
-            document.querySelector(".b2").classList.remove("act");
-            document.querySelector(".n4").classList.add("act");
-        };
-    } else if (h == 13) {
-        document.querySelector(".n4").classList.add("act");
-    } else if (h == 14) {
-        if (m < 30) {
-            document.querySelector(".n4").classList.add("act");
-        } else {
-            document.querySelector(".n4").classList.remove("act");
-            document.querySelector(".b3").classList.add("act");
-        };
-    } else if (h == 15 || h == 16) {
-        document.querySelector(".b3").classList.remove("act");
-        document.querySelector(".n5").classList.add("act");
-    } else if (h == 17) {
-        document.querySelector(".n5").classList.remove("act");
-        if (m < 30) {
-            document.querySelector(".b4").classList.add("act");
-        } else {
-            document.querySelector(".b4").classList.remove("act");
-            document.querySelector(".n61").classList.add("act");
-        };
-    } else if (h == 18) {
-        if (m < 30) {
-            document.querySelector(".n61").classList.add("act");
-        } else {
-            document.querySelector(".n61").classList.remove("act");
-            document.querySelector(".b51").classList.add("act");
-        };
-    } else if (h == 19) {
-        document.querySelector(".b51").classList.remove("act");
-        document.querySelector(".n62").classList.add("act");
-    } else if (h == 20) {
-        document.querySelector(".n62").classList.remove("act");
-        if (m < 30) {
-            document.querySelector(".b52").classList.add("act");
-        } else {
-            document.querySelector(".b52").classList.remove("act");
-            document.querySelector(".b53").classList.add("act");
-        };
-    } else if (h == 21 || h == 22) {
-        document.querySelector(".b53").classList.remove("act");
-        document.querySelector(".n7").classList.add("act");
+const ban = document.querySelector(".ban");
+const eng = document.querySelector(".eng");
+const eco = document.querySelector(".eco");
+const clg = document.querySelector(".clg");
+const ict = document.querySelector(".ict");
+const psy = document.querySelector(".psy");
+const civ = document.querySelector(".civ");
+const soc = document.querySelector(".soc");
+const sle = document.querySelector(".sle");
+const d = new Date();
+// Function;
+function perSecFunc() {
+  let hour = d.getHours();
+  let min = d.getMinutes();
+  // 3 AM;
+  if (hour == 3) {
+    if (min < 30) {
+      sle.classList.add("act");
     } else {
-        document.querySelector(".n7").classList.remove("act");
-    };
-};
-func();
-setInterval(func, 1000);
+      sle.classList.remove("act");
+      ban.classList.add("act");
+    }
+  }
+  // 4 AM;
+  if (hour == 4) {
+    ban.classList.add(act);
+  }
+  // 5 AM;
+  if (hour == 5) {
+    ban.classList.remove("act");
+    eng.classList.add("act");
+  }
+  // 6 AM;
+  if (hour == 6) {
+    if (min < 30) {
+      eng.classList.add("act");
+    } else {
+      eng.classList.remove("act");
+      eco.classList.add("act");
+    }
+  }
+  // 7 AM;
+  if (hour == 7) {
+    eco.classList.add("act");
+  }
+  // 8 AM - 2 PM;
+  if (hour >= 8 && hour < 15) {
+    eco.classList.remove("act");
+    clg.classList.add("act");
+  }
+  // 3 PM;
+  if (hour == 15) {
+    if (min < 30) {
+      clg.classList.add("act");
+    } else {
+      clg.classList.remove("act");
+      ict.classList.add("act");
+    }
+  }
+  // 4 PM;
+  if (hour == 16) {
+    ict.classList.remove("act");
+    psy.classList.add("act");
+  }
+  // 5 PM;
+  if (hour == 17) {
+    if (min < 30) {
+      psy.classList.add("act");
+    } else {
+      psy.classList.remove("act");
+      civ.classList.add("act");
+    }
+  }
+  // 6 PM;
+  if (hour == 18) {
+    civ.classList.add("act");
+  }
+  // 7 PM;
+  if (hour == 19) {
+      civ.classList.remove("act");
+      soc.classList.add("act");
+    }
+  // 8 PM;
+  if (hour == 20) {
+    if (min < 30) {
+      soc.classList.add("act");
+    } else {
+      soc.classList.remove("act");
+      sle.classList.add("act");
+    }
+  }
+}
+perSecFunc();
+setInterval(perSecFunc, 1000);
